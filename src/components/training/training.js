3534c4a34c088d4456462c7bee53028bae2ab34d
Joanna Kolbe <joanna.kolbe@rci.rogers.ca>
2018-11-13 21:17:17 -0500

    training

--- src/components/training/training.js
+++ src/components/training/training.js
@@ -11,13 +11,101 @@ class Training extends Component {
     this.timeline_items = null;
     this.state = {
       timeline: [
-        {title: 'University of Toronto', time: '1934', description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium'},
-        {title: 'University of Toronto', time: '1937', description: 'Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.'},
-        {title: 'University of Toronto', time: '1940', description: 'Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.'},
-        {title: 'University of Toronto', time: '1943', description: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.'},
-        {title: 'University of Toronto', time: '1956', description: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.'},
-        {title: 'University of Toronto', time: '1967', description: 'Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus, euismod quis ante.'},
-        {title: 'University of Toronto', time: '2005', description: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.'},
+        {
+          organization: 'Rogers Communications Inc.',
+          title: 'Sr. UI/UX Developer',
+          time: 'Nov. 2018 - present',
+          points: [
+            'Design and develop user interfaces',
+            'Build and integrate consistent, efficient components across the product portfolio',
+            'Prepare designs, documentation and use case scenarios',
+            'Plan and research usability and visual design for product'
+          ]
+        },
+        {
+          organization: 'University of Toronto',
+          title: 'Master of Information, ISD & UXD',
+          'time': 'Sept. 2014 - Present',
+          points: [
+            'Conduct research projects engaging with each stage of the UXD process - brainstorming, data gathering, analysis, designing, prototyping, and evaluation',
+            'Participate in multi-disciplinary teams to analyze, design, and implement innovative and effective information systems'
+          ]
+        },
+        {
+          organization: 'Sportsnet',
+          title: 'Sr. Web Developer',
+          'time': 'July 2017 - Nov. 2018',
+          points: [
+            'Create and maintain pages and plugins',
+            'Implement responsive web design',
+            'Improve effectiveness of agile development practices and processes',
+            'Monitor overall site stability and performance goals'
+          ]
+        },
+        {
+          organization: 'Rogers Communications Inc.',
+          title: 'Web Developer',
+          'time': 'June 2015 - July 2017',
+          points: [
+            'Create web interfaces for public facing websites',
+            'Design, develop and implement code supporting the customer experience',
+            'Create attractive and functional user interface interactions',
+            'Participate in project budgeting by providing estimates on development time for key projects'
+          ]
+        },
+        {
+          organization: 'CIBC',
+          title: 'Production Application Support Analyst',
+          'time': 'Jan. 2015 - May 2015',
+          points: [
+            'Evaluate service requests to ensure adherence to standards and processes',
+            'Identify and resolve production problems',
+            'Monitor and analyze supported services to identify opportunities for improvement',
+            'Implement changes to supporting production applications'
+          ]
+        },
+        {
+          organization: 'Set Scouter Inc.',
+          title: 'Web Developer',
+          'time': 'June 2014 - Feb. 2015',
+          points: [
+            'Develop new features and modules for current application',
+            'Integrate third party APIs with the system',
+            'Maintain pages within the WordPress content management system'
+          ]
+        },
+        {
+          organization: 'Digital Media Zone',
+          title: 'Project Lead and System Support Analyst',
+          'time': 'June 2014 - Jan. 2015',
+          points: [
+            'Supervise project development',
+            'Manage, design, build, and maintain software applications',
+            'Maintain MySQL database containing sensitive data',
+            'Provide statistics for grant reporting, monthly presentations, KPI monitoring, and business intelligence'
+          ]
+        },
+        {
+          organization: 'Digital Media Zone',
+          title: 'Web Developer',
+          'time': 'Sept. 2013 - June 2014',
+          points: [
+            'Build applications that streamline internal business process',
+            'Continually assess information system security and implement measures to protect data',
+            'Support the maintenance of SQL based databases',
+            'Continually assess information system security and implement measures to protect data'
+          ]
+        },
+        {
+          organization: 'Ryerson University',
+          title: 'Bachelor of Science, Computer Science',
+          'time': 'Sept. 2010 - June 2014',
+          points: [
+            'Acquire the skills and knowledge necessary to influence the hyperconnected digital landscape',
+            'Learn about data structures and algorithms, networks and operating systems',
+            'Study software engineering, computer vision, artificial intelligence and platform-based development'
+          ]
+        }
       ]
     }
 
@@ -68,14 +156,18 @@ class Training extends Component {
       <section id='training' className='Training__ctn'>
         <h2>TRAINING</h2>
         <div className='timeline'>
-          <ul>
+          <ul className='timeline_list'>
             {timeline.map((t, i) => (
-              <li key={`timeline${i}`}>
+              <li className='timeline_item' key={`timeline${i}`}>
                 <div className='timeline_element'>
-                  <h3 className='title'>{t.title}</h3>
+                  <h3 className='organization'>{t.organization}</h3>
                   <div className='content'>
+                    <p className='title'>{t.title}</p>
                     <time>{t.time}</time>
-                    {t.description}
+                    {t.points && <ul className='description'>
+
+                      {t.points.map(p => (<li>{p}</li>))}
+                    </ul>}
                   </div>
                 </div>
               </li>
