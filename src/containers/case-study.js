cfd9384e2880c95ba5fb5faad16475f09fe49502
Joanna Kolbe <joanna.kolbe@rci.rogers.com>
2018-11-28 09:59:53 -0500

    secondary research

--- src/containers/case-study.js
+++ src/containers/case-study.js
@@ -23,7 +23,7 @@ class CaseStudy extends Component {
     super();
     this.state = {
       steps: ['Research.', 'Analysis.', 'Design.', 'Evaluation.'],
-      appName: 'EventFun'
+      appName: 'Event Hunter'
     }
   }
 
@@ -53,19 +53,32 @@ class CaseStudy extends Component {
 
         <section className='project'>
           <h2>The project</h2>
-          <p>{appName} is an iOS application that aims to raise the awareness and promotion of local event
+          <p>{appName} is an iOS application that aims to raise awareness and promote local events
           by making them more accessible to the public. The product informs users about what is
           happening in their neighbourhood so they would never feel like they're missing out.
-          Users can search for a specific event or browse for events that match their preference criteria. {appName} awards points for event attendance and experience sharing that can later be used for discounts.</p>
+          Users can search for a specific event or browse for events that match their preference criteria. {appName} is a centralized application that combines events from all existing platforms.</p>
         </section>
 
         <section className='competitors'>
-          <h2>Competitor Analysis</h2>
+          <h2>Secondary Research</h2>
           <div className='section_split'>
             <div>
-              <p>Before beginning secondary research, we refined our focus to how people find out about local events. Through secondary research we aimed to answer the three aforementioned questions to learn more about our research topic.</p>
-              <p>We looked at existing platforms used to find and share events.  More specifically, we explored event sharing websites, social networking websites, articles, and blogs. We then used a consolidation table to summarize our findings. Our consolidation table included three aspects: what the platform is (observations), why it is important (interpretations), and what it means to us (implications).</p>
-              <p>A common theme among blogs and social networking sites is the use of weekly lists to share events. For instance, the Toronto subreddit on Reddit and the popular Toronto-based blog, BlogTO, both post weekly lists of events happening in the city. Facebook on the other hand is more social, as users can visit an event page, see events occurring in their area and invite others, and see events their friends plan on attending. Eventbrite displays different types of events happening in the Toronto area and allows users to filter by location, cost, and event type. Bulletin boards and word of mouth were the only ways to discover events offline we explored. </p>
+              <p>We conducted a secondary research to better understand event management domain. The analysis involved a variety of different products and event finding methods including Facebook, Word of mouth, Bulletin boards, EventBrite, BlogTO and Reddit.</p>
+              <p>Our research concluded with the following findings:</p>
+                <ul>
+                  <li>
+                    <span class='bold'>Many platforms</span>
+                    <span> - Users are forced to check multiple platforms to find events. This can be difficult and time consuming.</span>
+                  </li>
+                  <li>
+                    <span class='bold'>Listings vary</span>
+                    <span> - Lack of consistency between all the different platforms. Users are missing out on attending events due to lack of awareness.</span>
+                  </li>
+                  <li>
+                    <span class='bold'>Unequal exposure</span>
+                    <span> - Smaller events tend to be overshadowed by bigger ones due to the lack of proper advertisement. This can potentially lead to a low event attendance.</span>
+                  </li>
+                </ul>
             </div>
             <div>
               <img src={competitors} alt="fb, eventbrite, talking with friends, reddit, bulettin board, blogTo" />
