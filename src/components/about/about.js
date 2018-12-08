b68d8f19328df197d61228ba034713962e47ae92
Joanna Kolbe <joanna.kolbe@rci.rogers.com>
2018-12-08 09:34:50 -0500

    scroll top + favicon

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -16,7 +16,7 @@ class About extends Component {
             <div className='col-xs-12 col-sm-8'>
               <div className='profile'>
                 <h2>About me</h2>
-                <p>Hi! I'm Joana, a UX/UI developer based in Toronto and a student of Information program at University of Toronto.
+                <p>Hi! I'm Joanna, a UX/UI developer based in Toronto and a student of Information program at University of Toronto.
                 I am committed to building high quality, functional, and user-friendly experience that empowers its users,
                 and makes it easier for them to achieve their goals.</p>
                 <p>In a spare time I like to scuba dive and take care of my pup Zues.</p>
